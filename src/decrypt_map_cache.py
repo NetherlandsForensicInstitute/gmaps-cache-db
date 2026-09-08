be2a2639ba72d75f7ce4c5cb27dd427286a2a58c
Anne Fleur van Luenen <a.f.van.luenen@nfi.nl>
2026-09-08 09:53:27 +0200

    add epoch timestamp to geojson

--- src/decrypt_map_cache.py
+++ src/decrypt_map_cache.py
@@ -90,20 +90,19 @@ def get_tiles(db_path: Path, aes_key: bytes):
             # plain_data is not investigated further here
             metadata_msg, plain_data = decrypt_and_verify_tile(aes_key, metadata_nonce, metadata, data_nonce, data)
 
-            hashed_tile_key = hashed_tile_key.hex() if hashed_tile_key is not None else None
             z, x, y = [metadata_msg["TileKey"]["coordinate"][k] for k in ("zoom", "x", "y")]
             timestamp = datetime.fromtimestamp(priority / 1e3)
             shape = calc_tile_shape(z, x, y)
             # we use Google Tile grid indices for tile_id for legibility
             tile_id = f"{z}/{x}/{y}"
-            yield (timestamp, layer_id.decode("ascii"), shape, tile_id)
+            yield (timestamp, priority, layer_id.decode("ascii"), shape, tile_id)
 
 
 def get_tile_dataframe(key_path: Path, db_path: Path) -> GeoDataFrame:
     aes_key = get_aes_key(key_path)
     df = GeoDataFrame(
         get_tiles(db_path, aes_key),
-        columns=["timestamp", "layer_id", "shape", "tile_id"],
+        columns=["timestamp", "priority", "layer_id", "shape", "tile_id"],
         geometry="shape",
     )
     return df
